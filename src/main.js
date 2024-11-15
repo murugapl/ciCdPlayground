08063331705da4bea9e931422de8199fb30d4e61
Christof Happ <christof.happ@tngtech.com>
2024-11-15 10:53:08 +0100

    Introducing myself again

--- src/main.js
+++ src/main.js
@@ -1,28 +1,24 @@
-import App from './App.svelte';
+import App from "./App.svelte";
 
 new App({
-	target: document.body,
-	props: {
-		// What's your name?
-		name: 'Christof Happ',
-		// In the following fiels you can either give a single string, 
-		// or an array of bullet points
-		
-		// What do you associate with the term 'CI/CD'?
-		associations: [
-			"Oh, it's time again for the workshop?"
-		],
-		// Which CI/CD tools do you use in your project?
-		tools: "It's a bit sad",
-		// What do you want to learn in this workshop? 
-		expectations: [
-			'How this idea with the playground repo works'
-		],
-		// What do you like to do when you're not coding?
-		hobbies: [
-			'reading books', 
-			'cooking', 
-			'playing computer games'
-		]
-	}
+  target: document.body,
+  props: {
+    // What's your name?
+    name: "Christof Happ",
+    // In the following fiels you can either give a single string,
+    // or an array of bullet points
+
+    // What do you associate with the term 'CI/CD'?
+    associations: [
+      "Red tiles on a screen on the wall",
+      "Scripts in strange programming languages",
+      "Some annoying technology preventing me to finish my ticket and breaking production",
+    ],
+    // Which CI/CD tools do you use in your project?
+    tools: ":sadpanda:",
+    // What do you want to learn in this workshop?
+    expectations: ["Share knowledge about the basics of CI/CD"],
+    // What do you like to do when you're not coding?
+    hobbies: ["reading books", "cooking", "playing computer games"],
+  },
 });
