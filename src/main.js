ce8f47b985089339011d9053b3a7974ae41679d6
Lakshmi Murugappan <lakshmi.murugappan@tngech.com>
2025-01-24 13:34:48 +0000

    initial commit

--- src/main.js
+++ src/main.js
@@ -4,15 +4,16 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
+    name: "Lakshmi Murugappan",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["DevOps", "Continuous Integration", "Continuous Delivery"],
     // Which CI/CD tools do you use in your project?
     tools: "Jenkins, maybe?",
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: ["Learn more about CI/CD", "Learn how to use Jenkins"],
   },
 });
+ 
